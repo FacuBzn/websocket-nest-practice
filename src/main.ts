8cbd38adcac75f50db087fd1eddf980e944b58cc
FacuBzn <juanfacundobazan@gmail.com>
2025-01-19 20:12:52 -0300

    Refactor HTML structure and add client connection functionality

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,38 @@
 import './style.css'
 import { connectToServer } from './socket-cliente.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>('#root')!.innerHTML = `
   <div>
   <h1> Websocket - Client </h1>
+  <input id="jwt-token" placeholder="Json Web Token" />
+  <button id="btn-connect"> Connect </button>
 
   <spam id="server-status"> Offline </spam>
 
-  <ul id="clientes-ul">
+  <ul id="clients-ul">
     <li> Cliente Id </li>
+  </ul>
+
+  <form id="message-form">
+    <input placeholder="message" id="message-input" />    
+  </form>
+
+  <h3>  Messages </h3> 
+  <ul id="messages-ul">    
   </ul>
 
   </div>
 `
-connectToServer()
+/* 
+connectToServer() */
 /* setupCounter(document.querySelector<HTMLButtonElement>('#counter')!) */
+
+const jwtToken = document.querySelector<HTMLInputElement>('#jwt-token')!;
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect')!;
+
+btnConnect.addEventListener('click', () => {
+
+  if ( jwtToken.value.trim().length <= 0 ) return alert('Token is required');
+
+  connectToServer( jwtToken.value.trim() );
+})
--- src/socket-cliente.ts
+++ src/socket-cliente.ts
@@ -1,39 +1,94 @@
 import { Manager, Socket } from 'socket.io-client';
 
+let socket: Socket;
 
-export const connectToServer = () => {
-  const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
-  const socket = manager.socket('/');
-  addListeners(socket);
-  
+export const connectToServer = ( token: string) => {
+
+  const manager = new Manager('http://localhost:3000/socket.io/socket.io.js',{
+    extraHeaders: {
+      Authorization: `${token}`
+    }
+  });
+
+  socket?.removeAllListeners();
+  socket = manager.socket('/');
+  addListeners();  
 }
 
-const addListeners = (socket: Socket) => {
+const addListeners = () => {
 
+  const clientsUl = document.querySelector<HTMLUListElement>('#clients-ul')!;
+  const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
+  const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
+  const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')!;
   const serverStatusLabel = document.querySelector<HTMLSpanElement>('#server-status')!;
-  const clientesUl = document.querySelector<HTMLUListElement>('#clientes-ul')!;
+  const serverStatus = document.getElementById('server-status');
+
 
   socket.on('connect', () => {    
-    /* serverStatusLabel.textContent = 'Online' */
     serverStatusLabel.innerHTML = 'Online';
-    clientesUl.innerHTML = socket.id || '';
+    clientsUl.innerHTML = socket.id || '';
+    if (serverStatus) {
+      serverStatus.classList.add('connected');
+    }
   })
   
   socket.on('disconnect', () => {
-    console.log('Disconnected from server')
+    serverStatusLabel.innerHTML = 'Offline';
+    if (serverStatus) {
+      serverStatus.classList.remove('connected');
+    }
   })
 
   socket.on('clients-updated', (clients: string[]) => {
-    clientesUl.innerHTML = clients.map(client => `<li>${client}</li>`).join('')
+    clientsUl.innerHTML = clients.map(client => `<li>${client}</li>`).join('');
     /* let clientsHtml = '';
-    clients.forEach(client => {
-      clientsHtml += `<li>${client}</li>`;
-    });
-    clientesUl.innerHTML = clientsHtml; */
-
-    console.log({clients})
+        clients.forEach( clientId => {
+            clientsHtml += `
+                <li>${ clientId }</li>
+            `
+        });
+        clientsUl.innerHTML = clientsHtml; */
   })
   
+  messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if ( messageInput.value.trim().length <= 0 ) return;
+
+    socket.emit('message-from-client',{
+      /* id: socket.id, */
+      id: 'YO!',
+      message: messageInput.value
+    });
+
+    messageInput.value = '';
+  })
+
+  socket.on('message-from-server', (payload: { fullName:string, message:string }) => {
+    /* messagesUl.innerHTML += `<li> ${payload.message} </li>` */
+
+    const newMessage = 
+    `<li> 
+      <strong>${ payload.fullName }: </strong>
+      <spam>${ payload.message }</spam>
+    </li>`;
+
+    const li = document.createElement('li');
+    li.innerHTML = newMessage;
+    messagesUl.append(li);
+
+  })
+
+  serverStatus?.addEventListener('click', () => {
+    // Simular conexión
+    const isConnected = true; // Cambia esto según tu lógica de conexión
+
+    if (isConnected) {
+      serverStatus.classList.add('connected'); // Añade la clase 'connected'
+    } else {
+      serverStatus.classList.remove('connected'); // Quita la clase 'connected'
+    }
+  });
 
 }
 
